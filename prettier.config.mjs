c7440e59ecd908291a4faf8b182898dfc5c11e25
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-05-22 22:52:24 +0200

    Setup static code analysis

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+// Specify only the rules that cannot be defined within the .editorconfig file as Prettier inherits config from there.
+// See: https://prettier.io/docs/en/configuration.html#editorconfig
+
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: { parser: 'astro' },
+    },
+  ],
+};
+
+export default config;
