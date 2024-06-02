5b9085097e00cea905438615896a1729df01783c
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-02 21:06:00 +0200

    Customize markdown link renderer

--- src/utils/markdown-to-html.ts
+++ src/utils/markdown-to-html.ts
@@ -1,5 +1,20 @@
-import { marked } from 'marked';
+import { marked, Renderer } from 'marked';
+
+const renderer = new Renderer();
+
+renderer.link = (href, title, text) => {
+  const attributes = [
+    href && `href="${href}"`,
+    title && `title="${title}"`,
+    !href.startsWith('#') && 'target="_blank" rel="noopener noreferrer"',
+    'class="underline hover:opacity-75"',
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return `<a ${attributes}>${text}</a>`;
+};
 
 export function markdownToHtml(text: string) {
-  return marked.parse(text, { async: false }) as string;
+  return marked.parse(text, { async: false, renderer }) as string;
 }
