efa1e0564c6d6553e6ab2d060538c4ef375378b9
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-04 22:48:50 +0200

    Create Popover component

---
+++ src/utils/element-attributes.ts
@@ -0,0 +1,59 @@
+const TAG_WITH_ATTRIBUTES_REGEX = /<(\w+)([^>]*)>/;
+
+export function getElementAttributes(html: string) {
+  const result: Record<string, string | number | boolean> = {};
+
+  const attributesString = getAttributesString(html);
+
+  const attrRegex = /([a-zA-Z0-9-]+)(?:="([^"]*)")?/g;
+  let match;
+
+  while ((match = attrRegex.exec(attributesString)) !== null) {
+    const [, name = '', value] = match;
+
+    if (value === undefined) {
+      result[name] = false;
+    } else if (!isNaN(Number(value))) {
+      result[name] = Number(value);
+    } else if (value.toLowerCase() === 'true') {
+      result[name] = true;
+    } else if (value.toLowerCase() === 'false') {
+      result[name] = false;
+    } else {
+      result[name] = value;
+    }
+  }
+
+  return result;
+}
+
+export function addElementAttributes(html: string, attributes: Record<string, string | number | boolean>) {
+  const currentAttributes = getElementAttributes(html);
+  const newAttributes = { ...currentAttributes, ...attributes };
+
+  const attributesString = Object.entries(newAttributes)
+    .map(([name, value]) => {
+      if (value === true) return name;
+      if (value === false) return `${name}="false"`;
+      return `${name}="${value}"`;
+    })
+    .join(' ');
+
+  return html.replace(TAG_WITH_ATTRIBUTES_REGEX, `<${getTag(html)} ${attributesString}>`);
+}
+
+function getTag(html: string) {
+  const tagMatch = html.match(TAG_WITH_ATTRIBUTES_REGEX);
+  const tag = tagMatch?.[1];
+
+  if (!tag) {
+    throw new Error('Invalid HTML');
+  }
+
+  return tag;
+}
+
+function getAttributesString(html: string) {
+  const tagMatch = html.match(TAG_WITH_ATTRIBUTES_REGEX);
+  return tagMatch?.[2]?.trim() ?? '';
+}
