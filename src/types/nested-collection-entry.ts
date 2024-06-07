878b0b3eddf80563d25efcd289584e7ec7dedfdb
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-07 22:58:21 +0200

    Create articles section

--- src/types/nested-collection-entry.ts
+++
@@ -1,5 +0,0 @@
-import type { AnyEntryMap, CollectionEntry } from 'astro:content';
-
-export type NestedCollectionEntry<C extends keyof AnyEntryMap, P extends string> = CollectionEntry<C> & {
-  slug: `${P}/${string}`;
-};
