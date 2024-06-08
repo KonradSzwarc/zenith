6cd4405f0c7647e7a2630fa1910609818eee6d1a
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-08 22:33:28 +0200

    Move related components to the same folders

---
+++ src/components/favorites/index.ts
@@ -0,0 +1,1 @@
+export { default as Favorites } from './favorites.astro';
---
+++ src/components/skills/index.ts
@@ -0,0 +1,2 @@
+export { default as RelatedSkills } from './related-skills.astro';
+export { default as Skills } from './skills.astro';
