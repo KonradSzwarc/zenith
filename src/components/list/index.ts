d6027454a18645606777addcc775765a8b405059
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-17 20:22:58 +0200

    Move sections layout setup to index file for web

---
+++ src/components/list/index.ts
@@ -0,0 +1,3 @@
+export { default as GridList } from './grid-list.astro';
+export { default as HorizontalList } from './horizontal-list.astro';
+export { default as VerticalList } from './vertical-list.astro';
--- src/components/skills/index.ts
+++ src/components/skills/index.ts
@@ -1,2 +1,3 @@
 export { default as RelatedSkills } from './related-skills.astro';
-export { default as Skills } from './skills.astro';
+export { default as SkillBar } from './skill-bar.astro';
+export { default as SkillTag } from './skill-tag.astro';
