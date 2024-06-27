18971e19c5096d2e848a10c72b25e1bab6bc53dd
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-06-27 16:42:56 +0200

    Display footer only on the last page of PDF

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -31,56 +31,9 @@ async function generatePdf(name: string, page: Page) {
 
   await visitPage(INPUT_URL, name, page);
 
-  const footer = await getFooter(page);
-  const { margin } = await getPdfConfig(page);
-
   await page.pdf({
     path: `${OUTPUT_PATH}/${name}.pdf`,
     format: 'A4',
     printBackground: true,
-    margin,
-    displayHeaderFooter: Boolean(footer),
-    footerTemplate: footer,
-    headerTemplate: '<div></div>',
   });
-}
-
-function getFooter(page: Page) {
-  return page.evaluate(() => {
-    const footer = document.getElementById('footer');
-
-    if (!footer) {
-      return undefined;
-    }
-
-    // Casting required as some style properties (e.g. webkitPrintColorAdjust) are not recognized by TypeScript.
-    const footerStyle = footer.style as unknown as Record<string, unknown>;
-    const inlineStyles = JSON.parse(JSON.stringify(getComputedStyle(footer))) as object;
-
-    for (const [property, value] of Object.entries(inlineStyles)) {
-      footerStyle[property] = value;
-    }
-
-    footerStyle.margin = '0 auto';
-    footerStyle.webkitPrintColorAdjust = 'exact';
-
-    footer.removeAttribute('class');
-    const footerHtml = footer.outerHTML;
-    footer?.remove();
-
-    return footerHtml;
-  });
-}
-
-async function getPdfConfig(page: Page) {
-  const margin = await page.evaluate(() => {
-    return {
-      top: document.querySelector('meta[name="pdf-margin-top"]')?.getAttribute('content') ?? undefined,
-      right: document.querySelector('meta[name="pdf-margin-right"]')?.getAttribute('content') ?? undefined,
-      bottom: document.querySelector('meta[name="pdf-margin-bottom"]')?.getAttribute('content') ?? undefined,
-      left: document.querySelector('meta[name="pdf-margin-left"]')?.getAttribute('content') ?? undefined,
-    };
-  });
-
-  return { margin };
 }
